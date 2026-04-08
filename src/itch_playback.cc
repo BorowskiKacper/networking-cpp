477f6c4430c8bab7e934e4ed1ea855791ec820e4
Kacper Borowski <borowski.cacper@gmail.com>
2026-04-08 13:09:37 -0400

    update receiver and playback to multicast

--- src/itch_playback.cc
+++ src/itch_playback.cc
@@ -10,9 +10,12 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        std::cerr << "Usage: " << argv[0] << " <peer_ip> <peer_port>\n";
+        std::cerr << "Usage: " << argv[0] << " <multicast_ip> <port>\n"; // e.g., "239.0.0.1" 12345
         return EXIT_FAILURE;
     }
+
+    const char *multicast_ip = argv[1];
+    int port = atoi(argv[2]);
 
     std::ifstream file("itchmessages/12302019.NASDAQ_ITCH50", std::ios::binary);
 
@@ -24,17 +27,6 @@ int main(int argc, char *argv[])
 
     uint16_t msg_length;
 
-    const char *peer_ip = argv[1];
-    int peer_port = atoi(argv[2]);
-
-    struct sockaddr_in peer_addr = {.sin_family = AF_INET, .sin_port = htons(peer_port)};
-
-    if (inet_pton(AF_INET, peer_ip, &(peer_addr.sin_addr)) <= 0)
-    {
-        perror("Something went wrong with the IP address");
-        return EXIT_FAILURE;
-    }
-
     int udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (udp_fd < 0)
     {
@@ -42,24 +34,37 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    unsigned char ttl = 1;
+    if (setsockopt(udp_fd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl)) < 0)
+    {
+        perror("Setting ttl failed");
+        close(udp_fd);
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in multicast_addr = {.sin_family = AF_INET, .sin_port = htons(port)};
+    if (inet_pton(AF_INET, multicast_ip, &(multicast_addr.sin_addr.s_addr)) <= 0)
+    {
+        perror("Something went wrong with the IP address");
+        return EXIT_FAILURE;
+    }
+
     uint64_t i = 0;
 
-    while (file.read(reinterpret_cast<char *>(&msg_length), sizeof(msg_length))) // && i < 500)
+    while (file.read(reinterpret_cast<char *>(&msg_length), sizeof(msg_length)))
     {
         msg_length = ntohs(msg_length);
         std::cout << i << " | " << msg_length << std::endl;
 
         std::vector<char> msg_buffer(msg_length);
 
-        if (sendto(udp_fd, msg_buffer.data(), msg_length, 0, (struct sockaddr *)&peer_addr, sizeof(peer_addr)) < 0)
+        if (sendto(udp_fd, msg_buffer.data(), msg_length, 0, (struct sockaddr *)&multicast_addr, sizeof(multicast_addr)) < 0)
         {
             perror("Failed to send message");
             close(udp_fd);
             return EXIT_FAILURE;
         };
         i++;
-
-        // std::cout << "Sent \"" << message << "\" to " << peer_ip << ":" << peer_port << '\n';
     }
     close(udp_fd);
 
---
+++ src/itch_receiver.cc
@@ -0,0 +1,87 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <iostream>
+// #include <cstdlib>
+#include <cstring>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <multicast_ip> <port>\n"; // e.g., "239.0.0.1" 12345
+        return EXIT_FAILURE;
+    }
+
+    const char *multicast_ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_fd < 0)
+    {
+        perror("Sorry, couldn't create the socket");
+        return EXIT_FAILURE;
+    }
+
+    int reuse = 1;
+    if (setsockopt(udp_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0)
+    {
+        perror("Setting SO_REUSEADDR failed");
+        close(udp_fd);
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in local_addr =
+        {
+            .sin_family = AF_INET,
+            .sin_port = htons(port),
+            .sin_addr.s_addr = INADDR_ANY};
+
+    if (bind(udp_fd, (sockaddr *)&local_addr, sizeof(local_addr)) < 0)
+    {
+        perror("Binding to socket failed");
+        close(udp_fd);
+        return EXIT_FAILURE;
+    }
+
+    struct ip_mreq multicast_req = {.imr_interface.s_addr = INADDR_ANY};
+    if (inet_pton(AF_INET, multicast_ip, &(multicast_req.imr_multiaddr.s_addr)) <= 0)
+    {
+        perror("Something went wrong with the IP address");
+        close(udp_fd);
+        return EXIT_FAILURE;
+    }
+
+    if (setsockopt(udp_fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &multicast_req, sizeof(multicast_req)) < 0)
+    {
+        perror("Joining multicast group failed");
+        close(udp_fd);
+        return EXIT_FAILURE;
+    }
+
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in sender_addr;
+    socklen_t sender_len = sizeof(sender_addr);
+
+    while (true)
+    {
+        memset(buffer, 0, BUFFER_SIZE);
+        ssize_t received = recvfrom(udp_fd, &buffer, BUFFER_SIZE, 0, (sockaddr *)&sender_addr, &sender_len);
+
+        if (received < 0)
+        {
+            perror("Receive failed");
+        }
+        else
+        {
+            std::cout << received << std::endl;
+        }
+    }
+
+    close(udp_fd);
+
+    return EXIT_SUCCESS;
+}
