25ac66949adc02119705f4749c3f35ad6ca47b71
Kacper Borowski <borowski.cacper@gmail.com>
2026-04-08 08:05:54 -0400

    simple itch message blaster

---
+++ src/itch_playback.cc
@@ -0,0 +1,67 @@
+#include <unistd.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <arpa/inet.h>
+
+// namespace fh_lob
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <peer_ip> <peer_port>\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ifstream file("itchmessages/12302019.NASDAQ_ITCH50", std::ios::binary);
+
+    if (!file)
+    {
+        std::cout << "Failed to open file. " << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    uint16_t msg_length;
+
+    const char *peer_ip = argv[1];
+    int peer_port = atoi(argv[2]);
+
+    struct sockaddr_in peer_addr = {.sin_family = AF_INET, .sin_port = htons(peer_port)};
+
+    if (inet_pton(AF_INET, peer_ip, &(peer_addr.sin_addr)) <= 0)
+    {
+        perror("Something went wrong with the IP address");
+        return EXIT_FAILURE;
+    }
+
+    int udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_fd < 0)
+    {
+        perror("Sorry, couldn't create the socket");
+        return EXIT_FAILURE;
+    }
+
+    uint64_t i = 0;
+
+    while (file.read(reinterpret_cast<char *>(&msg_length), sizeof(msg_length))) // && i < 500)
+    {
+        msg_length = ntohs(msg_length);
+        std::cout << i << " | " << msg_length << std::endl;
+
+        std::vector<char> msg_buffer(msg_length);
+
+        if (sendto(udp_fd, msg_buffer.data(), msg_length, 0, (struct sockaddr *)&peer_addr, sizeof(peer_addr)) < 0)
+        {
+            perror("Failed to send message");
+            close(udp_fd);
+            return EXIT_FAILURE;
+        };
+        i++;
+
+        // std::cout << "Sent \"" << message << "\" to " << peer_ip << ":" << peer_port << '\n';
+    }
+    close(udp_fd);
+
+    return EXIT_SUCCESS;
+}
