b82a8908e2ce454eed7a97f7c4c245e4c27d1df7
Kacper Borowski <borowski.cacper@gmail.com>
2026-05-03 10:55:56 -0400

    create functions that introduce inline assembly instructions to read the time stamp counter

---
+++ include/rdtsc.h
@@ -0,0 +1,4 @@
+#include <arpa/inet.h>
+
+static inline uint64_t rdtsc_start();
+static inline uint64_t rdtsc_end();
---
+++ src/rdtsc.cc
@@ -0,0 +1,27 @@
+#include <arpa/inet.h>
+
+#include "../include/rdtsc.h"
+
+static inline uint64_t rdtsc_start()
+{
+    uint32_t low, high;
+    asm volatile(
+        "lfence\n\t"
+        "rdtsc\n\t"
+        : "=a"(low), "=d"(high)
+        :
+        : "memory");
+    return ((uint64_t)high << 32) | low;
+}
+
+static inline uint64_t rdtsc_end()
+{
+    uint32_t low, high;
+    asm volatile(
+        "rdtscp\n\t"
+        "lfence"
+        : "=a"(low), "=d"(high)
+        :
+        : "memory", "rcx");
+    return ((uint64_t)high << 32) | low;
+}
